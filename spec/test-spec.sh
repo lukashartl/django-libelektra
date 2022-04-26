c7e6daa723f25abe40909b1407661ecc62778ef8
Lukas Hartl <git@lukashartl.at>
2022-04-26 21:24:01 +0200

    spec: add working test data

--- spec/test-spec.sh
+++ spec/test-spec.sh
@@ -16,20 +16,24 @@ kdb set user:$CURRENT_PATH/allowed_hosts/\#0 'localhost'
 kdb meta-set user:$CURRENT_PATH/csrf_origins array '#0'
 test $(kdb get $CURRENT_PATH/csrf_origins/\#0) == 'localhost'
 
-kdb meta-set spec:$CURRENT_PATH/databases array '#0'
-kdb set user:$CURRENT_PATH/databases/\#0/name 'mydb'
+kdb meta-set user:$CURRENT_PATH/databases array '#0'
+kdb set user:$CURRENT_PATH/databases/\#0 'default'
 test $(kdb get $CURRENT_PATH/databases/\#0/engine) == 'django.db.backends.sqlite3'
 
-kdb set user:$CURRENT_PATH/databases/\#0/engine 'django.db.backends.oracle'
-kdb set user:$CURRENT_PATH/databases/\#0/user 'toad'
-kdb set user:$CURRENT_PATH/databases/\#0/password 'xyz'
+kdb set user:$CURRENT_PATH/databases/\#0/engine 'django.db.backends.sqlite3'
+kdb set user:$CURRENT_PATH/databases/\#0/name 'my_cool_db.sqlite3'
+kdb set user:$CURRENT_PATH/databases/\#0/user ''
+kdb set user:$CURRENT_PATH/databases/\#0/password ''
+kdb set user:$CURRENT_PATH/databases/\#0/port 0
 
 err=0
 kdb set user:$CURRENT_PATH/databases/\#0/engine 'not.an.engine' || err=1
 test $err -ne 0
 
-kdb meta-set user:$CURRENT_PATH/apps array '#2'
-kdb set user:$CURRENT_PATH/allowed_hosts/\#0 'myapp1'
-kdb set user:$CURRENT_PATH/allowed_hosts/\#1 'myapp2'
-kdb set user:$CURRENT_PATH/allowed_hosts/\#2 'myapp3'
-
+kdb meta-set user:$CURRENT_PATH/apps array '#5'
+kdb set user:$CURRENT_PATH/apps/\#0 'django.contrib.admin'
+kdb set user:$CURRENT_PATH/apps/\#1 'django.contrib.auth'
+kdb set user:$CURRENT_PATH/apps/\#2 'django.contrib.contenttypes'
+kdb set user:$CURRENT_PATH/apps/\#3 'django.contrib.sessions'
+kdb set user:$CURRENT_PATH/apps/\#4 'django.contrib.messages'
+kdb set user:$CURRENT_PATH/apps/\#5 'django.contrib.staticfiles'
